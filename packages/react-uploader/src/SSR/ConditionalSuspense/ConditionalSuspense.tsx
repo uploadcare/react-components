63a62539bbb9e561939fb76c1d90112dc8d92bc6
Egor Didenko <egor.didenko@list.ru>
2024-06-11 13:12:46 -0400

    fix(suspense): Fixed the implementation to apiRef.
    Now apiRef works.
    Note:
    1. When using Next.js you need to specify “use-client”
    2. In Next.js should solve issues with hydration and SSR support via React Uploader

--- packages/react-uploader/src/SSR/ConditionalSuspense/ConditionalSuspense.tsx
+++ packages/react-uploader/src/SSR/ConditionalSuspense/ConditionalSuspense.tsx
@@ -1,17 +1,15 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense } from "react";
 
 export type TProps = {
-    fallback?: React.ReactChild | React.ReactFragment | React.ReactPortal | null;
-    condition: boolean;
-    children: React.ReactNode;
+  // fallback?: React.ReactChild | React.ReactFragment | React.ReactPortal | null;
+  // condition: boolean;
+  children: React.ReactNode;
 };
 
-export const ConditionalSuspense: FC<TProps> = ({ condition, fallback, children, ...rest }) => {
-    return condition ? (
-        <Suspense fallback={fallback} {...rest}>
-            {children}
-        </Suspense>
-    ) : (
-        <>{fallback}</>
-    );
+export const ConditionalSuspense: FC<TProps> = ({
+  // condition,
+  // fallback,
+  children,
+}) => {
+  return children;
 };
